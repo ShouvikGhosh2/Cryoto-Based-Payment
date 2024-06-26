c328f3930620b5fcdd15abb5ec11e4fdcd13ee2f
Shouvik Kumar Ghosh <130725715+ShouvikGhosh2@users.noreply.github.com>
2024-06-26 21:07:45 +0530

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ee2fdbf804459b38a9a9b499fc85ce37a1b5ff83',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ee2fdbf804459b38a9a9b499fc85ce37a1b5ff83',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'beycanpress/cryptopay-integrator' => array(
+            'pretty_version' => '0.1.11',
+            'version' => '0.1.11.0',
+            'reference' => 'cd50c21a0daf9bbcc5b86a292d620b74b47aa73e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../beycanpress/cryptopay-integrator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
